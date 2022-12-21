2e093d1f37f6e3c35d38e14cc753117f1ea3050f
albuquerque-lucas <lucaslpra@gmail.com>
2022-12-21 15:22:56 -0300

    Tentativa de resolver requisito 5

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,14 +2,16 @@ window.onload = () => {
 
     const btnGenerateColors = document.querySelector('#button-random-color');
     const colorList = document.querySelectorAll('.color');
-    triggerPaletteButton(btnGenerateColors, colorList);
     fetchSavedColors(colorList);
+    triggerPaletteButton(btnGenerateColors, colorList);
+
 
 }
 
 const triggerPaletteButton = (button, target) => {
 
     button.addEventListener('click', event => {
+        event.preventDefault();
         generateColors(target);
     });
 
@@ -22,18 +24,18 @@ const generateColors = target => {
         const colorTwo = `rgb(${generateRGBNumber()})`;
         const colorThree = `rgb(${generateRGBNumber()})`;
 
-        const colorArray = [colorZero, colorOne, colorTwo, colorThree];
+        const colorPalette = [colorZero, colorOne, colorTwo, colorThree];
+        const colorPalette2 = JSON.stringify(colorPalette);
 
 
-        localStorage.setItem('colorPalette', colorArray);
+        localStorage.setItem('colorPalette', colorPalette);
         localStorage.setItem('colorGeneratedZero', colorZero);
         localStorage.setItem('colorGeneratedOne', colorOne);
         localStorage.setItem('colorGeneratedTwo', colorTwo);
         localStorage.setItem('colorGeneratedThree', colorThree);
 
-
         for(let i = 1; i < target.length; i += 1){
-            target[i].style.backgroundColor = colorArray[i];
+            target[i].style.backgroundColor = colorPalette[i];
         }
 }
 
@@ -42,7 +44,15 @@ const generateRGBNumber = () => {
     let colorIndexOne = Math.floor(Math.random() * 255);
     let colorIndexTwo = Math.floor(Math.random() * 255);
     let colorIndexThree = Math.floor(Math.random() * 255);
-    const rgbCode = [colorIndexOne, colorIndexTwo, colorIndexThree].toString();
+
+    const rgbCode = `${colorIndexOne}, ${colorIndexTwo}, ${colorIndexThree}`;
+    const rgbCode2 = JSON.stringify(colorIndexOne, colorIndexTwo, colorIndexThree);
+
+    let stringifiedOne = JSON.stringify(colorIndexOne);
+    let stringifiedTwo = JSON.stringify(colorIndexTwo);
+    let stringifiedThree = JSON.stringify(colorIndexThree);
+
+    const final = `${stringifiedOne}, ${stringifiedTwo}, ${stringifiedThree}`;
 
     return rgbCode;
 }
